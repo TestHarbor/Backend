ccaad74e9100004ae0a9bf2801b4d98394538dfd
ye0ngjae <op@ye0ngjae.com>
2023-11-09 09:57:49 +0900

    DB CONNECT✅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FileModule } from './file/file.module';
+import { ConfigModule } from '@nestjs/config';
+import { DataBaseModule } from './database.module';
 
 @Module({
-  imports: [FileModule],
+  imports: [FileModule, DataBaseModule, ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
